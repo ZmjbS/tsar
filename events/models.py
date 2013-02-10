40da5de7939f1a3844339a3367ac98c68b374215
Martin Swift <martin@swift.is>
2013-02-10 19:05:04 +0000

    Added some comments to the events models.

--- events/models.py
+++ events/models.py
@@ -9,12 +9,14 @@ from django.forms.extras.widgets import SelectDateWidget
 from django.forms.widgets import SplitDateTimeWidget
 
 class Role(models.Model):
+	# The roles that participatns in an event may have (e.g. driver, organiser, instructor, etc.).
 	title = models.CharField(max_length=64)
 
 	def __unicode__(self):
 		return self.title
 
 class EventType(models.Model):
+	# Evetns may be of several different types that one may like to distinguish between (e.g. exercise, course, meeting, social, etc.). One may even wish to serve different sets of tags for each.
 	title = models.CharField(max_length=64)
 	description = models.TextField(blank=True)
 
@@ -22,19 +24,30 @@ class EventType(models.Model):
 		return self.title
 
 class Event(models.Model):
+	# The fundamental object. Most of the magic happens in EventRole, however.
+	# An event may have several roles and members can be invited to one or more roles, either as members, or through groups which they belong to.
+	# Events come in various types, have locations and can be tagged with techniques employed or something similar that will aid in diagnosing events.
 	title = models.CharField(max_length=64)
 	description = models.TextField(blank=True)
 	date_time_begin = models.DateTimeField()
 	date_time_end = models.DateTimeField()
-	#Need to implement locations. Character fields or just store this all in a GPX file?
-	#Should locations be tags, or is it sufficient to filter from predefined areas and GPX files?
+	# TODO: Need to implement locations. Character fields or just store this all in a GPX file?
 	event_type = models.ForeignKey(EventType)
-	#Add equipment to log equipment use. Better to add this as a relation rather than a tag?
+	# TODO: Add equipment to log equipment use. Better to add this as a relation rather than a tag?
 
 	def __unicode__(self):
 		return self.title
 
 class EventRole(models.Model):
+	# An event may have several roles, and the same group or member may be invited to each of these. So for each role on each event we need an “event role”.
+	# These are associated with:
+	# * an event
+	# * a role
+	# * (optionally) one or more groups through GroupInvitation
+	# * (optionally) one or more members through MemberInvitation
+	# An event role may have a minimum or maximum number of participants and can even be open to anyone who may wish to join.
+	# TODO: Open/closed events. Simple boolean?
+	# TODO: The “hidden” parameter should probably be here, rather than in the intermediary models.
 	event = models.ForeignKey(Event)
 	role = models.ForeignKey(Role)
 	invited_groups = models.ManyToManyField(Group, through='GroupInvitation')
@@ -46,6 +59,7 @@ class EventRole(models.Model):
 		return self.event.title +' '+ self.role.title
 
 class GroupInvitation(models.Model):
+	# An intermediary model between an event role and a group.
 	event_role = models.ForeignKey(EventRole)
 	group = models.ForeignKey(Group)
 	is_hidden = models.BooleanField(default=False)
@@ -54,6 +68,7 @@ class GroupInvitation(models.Model):
 		return self.event_role.__unicode__() +'>'+ self.group.title
 
 class MemberInvitation(models.Model):
+	# An intermediary model between an event role and a member.
 	event_role = models.ForeignKey(EventRole)
 	member = models.ForeignKey(Member)
 	is_hidden = models.BooleanField(default=False)
@@ -61,7 +76,7 @@ class MemberInvitation(models.Model):
 	def __unicode__(self):
 		return self.event_role.__unicode__() +'>'+ self.member.user.username
 
-#class SplitDateTimeJSField(forms.SplitDateTimeField):
+# TODO: Need to implement some sort of confirmation class.
 
 class EventCreation(ModelForm):
 	class Meta:
