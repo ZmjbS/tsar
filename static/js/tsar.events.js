cf6fb0a902dbe62cf4074edbce6bb274d6a2e490
Martin Swift <martin@swift.is>
2013-08-25 20:31:58 +0000

    Moved some code from templates.

--- static/js/tsar.events.js
+++ static/js/tsar.events.js
@@ -4,6 +4,14 @@
  *
  */
 
+	$('.delete-role-entry').live('click',(function(){
+		role_id = $(this).attr('id');
+		role_title = $(this).parents('.role_entry').find('.role_entry-title').html();
+		$(this).parents('.role_entry').fadeOut(300).slideUp({ duration: 500, queue: false });
+		$(this).parents('.role_entry').remove();
+		$('#role-dropdown').append('<li id="'+role_id+'" class="role-dropdown-item"><a>'+role_title+'</a></li>');
+	}));
+
 /* Add functions that apply to elements on the page */
 $(document).ready(function(){
 
@@ -19,18 +27,38 @@ $(document).ready(function(){
 		var posting = $.post("/vidburdur/nyskraning/vista", {'csrfmiddlewaretoken': csrf, 'data': json});
 		posting.done(function(data) {
 			console.log('posting done!');
-			if (isNaN(parseInt(data))) {
-				$('#event_id').val(data);
-				print_result('Error: '+data,'alert-error');
-			} else {
-				console.log('Is a number. Redirecting...');
-				location.href = "/vidburdur/"+data;
-		//		print_result('Viðburður númer '+data+' búinn til. Sjá <a href="/vidburdur/'+data+'">síðu viðburðarins</a>','alert-success');
+			response = JSON && JSON.parse(data) || $.parseJSON(data);
+			switch (response['type']) {
+				case 'success':
+					console.log('SUCCESS!!!');
+					console.log('response[type]: '+response['type']);
+					console.log('response[event_id]: '+response['event_id']);
+					location.href = "/vidburdur/"+response['event_id'];
+					//print_result('Viðburður númer '+data+' búinn til eða lagaður.','alert-success');
+					break;
+				case 'error':
+					console.log('ERROR.');
+					print_result(response['message'], 'alert-error');
+					break;
+				default:
+					console.log('response: '+data);
+					console.log('response[type]: '+response['type']);
+					print_result('Unfamiliar response: '+response['type']+'. Message: '+response['message'], 'alert-error');
+					break;
 			}
 		})
 		.fail(function() { console.log("error"); })
 		.always(function() { console.log("finished"); });
 		return false;
 	});
+
+	function print_result(message,type){
+		$('#results').slideDown()
+		$('#results').html(message)
+		if (type) {
+			//$('#results').removeClass()
+			$('#results').addClass('alert '+type)
+		}
+	}
 
 }); // end document ready
