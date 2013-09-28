f2f2fb3e9122333663a585ef8c0c750de4509895
Martin Swift <martin@swift.is>
2013-09-28 14:27:47 +0000

    Simplified a bit.

--- events/urls.py
+++ events/urls.py
@@ -2,20 +2,20 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import DetailView, ListView
 from events.models import Event
 
-urlpatterns = patterns('',
-	url(r'^$', 'events.views.list_events', name='events'),
+urlpatterns = patterns('events.views',
+	url(r'^$', 'list_events', name='events'),
 	#url(r'^(?P<pk>\d+)/$',
 	#	DetailView.as_view(
 	#		model=Event,template_name='events/event_page.html')
 	#	),
-	url(r'^(?P<pk>\d+)/$','events.views.display_event', name='events'),
-	url(r'^(?P<pk>\d+)/svara$','events.views.event_response', name='events'),
-	url(r'^svara$','events.views.event_response', name='events'),
-	url(r'^svaranytt$','events.views.event_response_new', name='events'),
+	url(r'^(?P<pk>\d+)/$','display_event', name='events'),
+	url(r'^(?P<pk>\d+)/svara$','event_response', name='events'),
+	url(r'^svara$','event_response', name='events'),
+	url(r'^svaranytt$','event_response_new', name='events'),
 	#url(r'^(?P<pk>\d+)/breyta',
 	#	DetailView.as_view(
 	#		model=Event,template_name='events/create_event.html')
 	#	),
-	url(r'^nyskraning/$', 'events.views.display_or_save_event_form', name='events'),
-	url(r'^nyskraning/vista$', 'events.views.save_event', name='events'),
+	url(r'^nyskraning/$', 'display_or_save_event_form', name='events'),
+	url(r'^nyskraning/vista$', 'save_event', name='events'),
 )
