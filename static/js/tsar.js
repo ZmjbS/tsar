49ce58c973fb04894f2d72fcd872669a08d88821
Martin Swift <martin@swift.is>
2014-05-28 12:57:31 +0000

    Moving getnews to javascript file.

--- static/js/tsar.js
+++ static/js/tsar.js
@@ -48,6 +48,39 @@ function checkin(button, pagetype) {
 	return false;
 };
 
+function getnews(sel, url, num) {
+	/* Takes three arguments:
+	 *   sel - the selector where the unordered list of items should be added
+	 *   url - the URL of the RSS stream
+	 *   num - the number of news items that should be returned (optional)
+	 */
+	num = typeof num !== 'undefined' ? num : 8;
+	var newsdata = $.get("/news/", { url: url, num: num, }, function(data) {
+		obj = JSON && JSON.parse(data) || $.parseJSON(data);
+		console.log( "success"+data);
+		console.log(obj);
+		console.log( "success"+obj[0].title);
+		list = ""
+		for (count in obj) {
+			entry = obj[count]
+			console.log(entry.title)
+			list = list+'<li><a href="'+ entry.link +'">'+ entry.title +'</a> <time datetime="'+entry.published+'">'+ entry.published_parsed +'</time></li>';
+		}
+		$(sel).html('<ul>'+list+'</ul>');
+		})
+		.done(function() {
+			console.log( "second success" );
+		})
+		.fail(function() {
+			console.log( "error" );
+		})
+		.always(function(data) {
+			obj = JSON && JSON.parse(data) || $.parseJSON(data);
+			console.log( "finished" );
+		});
+		console.log('posted')
+}
+
 $(document).ready(function(){
 //$('.nav a[href*="' + location.pathname + '"][class!="noselect"]').addClass('selected');
 	$('.nav li a[href="' + location.pathname + '"][class!="noselect"]').addClass('selected');
