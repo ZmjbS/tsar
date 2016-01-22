3aaa117b361460895ee8e07469bc49a7da6d297b
Martin Swift <martin@swift.is>
2016-01-22 14:14:46 +0000

    Smávægileg breyting á nafni gagnagrunns.

--- tsar/settings.py
+++ tsar/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'tsar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
